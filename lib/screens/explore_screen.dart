fb97d21f7eeb9a0ed3d0bb3fdbf0159b5063c6a8
yen <yen.work.0318@gmail.com>
2022-06-19 17:53:45 +0800

    add receipt list in explore page

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_receipt/api/mock_fooderlich_service.dart';
+import 'package:flutter_receipt/components/today_recipe_list_view.dart';
+import 'package:flutter_receipt/models/explore_data.dart';
+
 
 class ExploreScreen extends StatelessWidget {
   final mockService = MockFooderlichService();
+
   ExploreScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: mockService.getExploreData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return const Center(child: Text('Show TodayRecipeListView'));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        });
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
