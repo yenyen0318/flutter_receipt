c9f13e2be68e7e5972dd24abc71e916f4e602207
yen <yen.work.0318@gmail.com>
2022-06-20 21:17:17 +0800

    add explore scrollListener when reached bottom/top

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_receipt/api/mock_fooderlich_service.dart';
-import 'package:flutter_receipt/components/today_recipe_list_view.dart';
+import 'package:flutter_receipt/components/components.dart';
 import 'package:flutter_receipt/models/explore_data.dart';
 
-import '../components/components.dart';
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
 
-class ExploreScreen extends StatelessWidget {
+class _ExploreScreenState extends State<ExploreScreen> {
   final mockService = MockFooderlichService();
+  late ScrollController _controller;
 
-  ExploreScreen({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      debugPrint('reached the bottom');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      debugPrint('reached the top!');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +45,7 @@ class ExploreScreen extends StatelessWidget {
         builder: (context, AsyncSnapshot<ExploreData> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView(
+              controller: _controller,
                 scrollDirection: Axis.vertical,
                 children: [
                   TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
