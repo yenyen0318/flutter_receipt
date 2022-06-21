5476d9df0bf661e292ae895712c33445fdbfa0f6
yen <yen.work.0318@gmail.com>
2022-06-21 16:28:22 +0800

    add empty grocery screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_receipt/screens/empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO 2: Replace with EmptyGroceryScreen
-    return Container(color: Colors.green);
+    return const EmptyGroceryScreen();
+
   }
 }
